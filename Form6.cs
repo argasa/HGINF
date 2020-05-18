42b497a3d55da508fea105928ee64319b8e30d81
mva <mva@W-001>
2020-05-18 15:37:41 +0700

    все

---
+++ Form6.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace HGINF
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                File.Delete("pcnum.conf");
+                File.WriteAllText("pcnum.conf", maskedTextBox1.Text);
+                this.Close();
+        }
+    }
+}
---
+++ RequestContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace HGINF
+{
+    class RequestContext: DbContext
+    {
+        public RequestContext()
+           : base("DbConnection")
+        { }
+
+        public DbSet<Request> Requests { get; set; }
+    }
+}
