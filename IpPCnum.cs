e4ece14f67bcfdfb4fddce9e7843ae17d2c09c5d
mva <mva@W-001>
2020-06-14 15:37:45 +0700

    й

---
+++ IpPCnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HGINF
+{
+    class IpPCnum
+    {
+        public int Id { get; set; }
+        public string IP { get; set; }
+        public string PCnum { get; set; }
+    }
+}
